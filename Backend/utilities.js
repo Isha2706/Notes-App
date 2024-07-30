c85f6e4bf8f10eb706cbf64290a95b4a6d2114ae
Amey1004 <amey.dev1111@gmail.com>
2024-07-31 03:27:06 +0530

    Add initial file

---
+++ Backend/utilities.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+
+function authenticateToken(req,res,next) {
+   const authHeader = req.headers["authorization"];
+   const token = authHeader && authHeader.split(" ")[1];
+
+   if (!token) return res.sendStatus(401);
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(401)
+    req.user = user;
+    next();
+   });
+}
+
+module.exports = {authenticateToken,};
+
+
+// const jwt = require('jsonwebtoken');
+
+// function authenticateToken(req, res, next) {
+//   // Changed from req.header to req.headers
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+
+//   if (!token) return res.status(401).json({ error: true, message: "No token provided" });
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if (err) return res.status(403).json({ error: true, message: "Invalid or expired token" });
+//     req.user = user;
+//     next();
+//   });
+// }
+
+// module.exports = { authenticateToken };
